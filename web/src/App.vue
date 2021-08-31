c6c30cd3888fe442c75a1860250e9a8f4de8bd92
Gavin Chain <321841642@qq.com>
2021-08-31 14:23:31 +0800

    wip: first

---
+++ web/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    基地组织
+    <div>
+      vue3+单向数据流,微服务
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
